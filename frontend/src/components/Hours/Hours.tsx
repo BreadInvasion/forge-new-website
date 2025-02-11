5800636e8d472214c139cd0d6db96242f3541993
monicalu <monicalu1128@hotmail.com>
2025-02-11 16:30:35 -0500

    Change Google Calendar iframe Link
    -Change the iframe link to the forge google calendar link

--- frontend/src/components/Hours/Hours.tsx
+++ frontend/src/components/Hours/Hours.tsx
@@ -4,7 +4,7 @@ const Hours = () => {
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>
       <iframe
-        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FNew_York&showPrint=0&mode=WEEK&src=bW9uaWNhbHUxMTI4QGdtYWlsLmNvbQ"
+        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FNew_York&showPrint=0&src=MDloMnRqZGZvZm1tcHBiMDhsbTZ1ZG5kbW9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=aThxdmFodDE1aHFzdjlpbDFyNjdmNnV2NzhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=azJyNm9zamptczZscXQ0MWJpNWE3ajQ4bjBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23D50000&color=%23795548&color=%23F4511E"
         style={{ border: "solid 1px #777" }}
         width="800"
         height="600"
@@ -16,3 +16,19 @@ const Hours = () => {
 };
 
 export default Hours;
+
+/*
+import React, {ReactNode, SyntheticEvent} from 'react';
+import ApiCalendar from "react-google-calendar-api";
+
+const config = {
+  clientId: "<CLIENT_ID>",
+  apiKey: "<API_KEY>",
+  scope: "https://www.googleapis.com/auth/calendar",
+  discoveryDocs: [
+    "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+  ],
+};
+
+const apiCalendar = new ApiCalendar(config);
+*/
