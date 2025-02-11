ed5a8ef268439dccc865d72ac73d22129da12745
monicalu <monicalu1128@hotmail.com>
2025-02-11 17:35:17 -0500

    Clean up Comments

--- frontend/src/components/Hours/Hours.tsx
+++ frontend/src/components/Hours/Hours.tsx
@@ -16,19 +16,3 @@ const Hours = () => {
 };
 
 export default Hours;
-
-/*
-import React, {ReactNode, SyntheticEvent} from 'react';
-import ApiCalendar from "react-google-calendar-api";
-
-const config = {
-  clientId: "<CLIENT_ID>",
-  apiKey: "<API_KEY>",
-  scope: "https://www.googleapis.com/auth/calendar",
-  discoveryDocs: [
-    "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
-  ],
-};
-
-const apiCalendar = new ApiCalendar(config);
-*/
