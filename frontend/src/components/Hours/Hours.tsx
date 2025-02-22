bf9572d84639d7eef4e4a4cb52e3b5e854a6df2a
monicalu <monicalu1128@hotmail.com>
2025-02-21 21:30:22 -0500

    Fix the issue showing personal gmail account & Change iframe link
    -Fix the issue showing personal gmail account
    -Change iframe link

--- frontend/src/components/Hours/Hours.tsx
+++ frontend/src/components/Hours/Hours.tsx
@@ -4,7 +4,7 @@ const Hours = () => {
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>
       <iframe
-        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FNew_York&showPrint=0&mode=WEEK&src=bW9uaWNhbHUxMTI4QGdtYWlsLmNvbQ&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=MDloMnRqZGZvZm1tcHBiMDhsbTZ1ZG5kbW9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=aThxdmFodDE1aHFzdjlpbDFyNjdmNnV2NzhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=azJyNm9zamptczZscXQ0MWJpNWE3ajQ4bjBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23039BE5&color=%2333B679&color=%23D50000&color=%23795548&color=%23F4511E"
+        src="https://calendar.google.com/calendar/b/1/embed?height=800&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&src=MDloMnRqZGZvZm1tcHBiMDhsbTZ1ZG5kbW9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=aThxdmFodDE1aHFzdjlpbDFyNjdmNnV2NzhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=azJyNm9zamptczZscXQ0MWJpNWE3ajQ4bjBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%231F753C&color=%2330487E&color=%238A2D38&mode=WEEK&showNav=0&showTitle=0&showDate=0&showPrint=0&showTabs=0&showCalendars=0"
         style={{ border: "solid 1px #777" }}
         width="800"
         height="600"
