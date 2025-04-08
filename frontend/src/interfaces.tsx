ec88793edb385d5a78fa24e52d20ec6cb090b467
Xenia Khusid <75655496+3791xk@users.noreply.github.com>
2025-04-07 23:25:56 -0400

    My Forge Summary Page (#11)
    This is the user's account summary page. Semester cost does pull the
    user's semester cost variable, mine is just incorrectly 0 bc there is no
    semester. Current usage combines a user's machine usages with machine
    status to find the current usage. The pie charts should be accurate
    based on machine usage data. Join our discord is a real link.
    
    
    ![image](https://github.com/user-attachments/assets/c9689e87-aaeb-4ce6-bb92-dae012324292)
    
    **Notes:** 
    * Semester Cost doesn't work because we've been unable to create
    semesters due to Teapot reasons.
    * The usage history is just to show the data is there, once the reusable
    table component exists, this will get replaced with a nice table

--- frontend/src/interfaces.tsx
+++ frontend/src/interfaces.tsx
@@ -1,6 +1,6 @@
 // src/interfaces.ts
 
-export interface User extends Record<string, any> {
+export interface User {
     id: string;
     is_rpi_staff: boolean;
     RCSID: string;
@@ -11,6 +11,7 @@ export interface User extends Record<string, any> {
     gender_identity: string;
     pronouns: string;
     role_ids: string[];
+    display_role: string;
     is_graduating: boolean;
     semester_balance: string;
 };
@@ -26,11 +27,12 @@ export const defaultUser: User = {
     gender_identity: "notdisclosed",
     pronouns: '',
     role_ids: [],
+    display_role: '',
     is_graduating: false,
     semester_balance: '',
 };
 
-export interface Machine extends Record<string, any> {
+export interface Machine {
     id: string;
     name: string;
     group: string;
@@ -52,7 +54,7 @@ export const emptyMachine: Machine = {
     disabled: false,
 };
 
-export interface MachineType extends Record<string, any> {
+export interface MachineType {
     id: string;
     name: string;
     resource_slots: number;
@@ -61,13 +63,21 @@ export interface MachineType extends Record<string, any> {
     resource_types: string[];
 };
 
-export interface MachineGroup extends Record<string, any> {
+export interface MachineGroup {
     id: number;
     name: string;
     machines: string[];
 };
 
-export interface Resource extends Record<string, any> {
+export interface MachineUsage {
+    semester: string
+    time_started: Date
+    duration: number
+    machine_name: string
+    cost: number
+};
+
+export interface Resource {
     id: string;
     name: string;
     brand?: string;
