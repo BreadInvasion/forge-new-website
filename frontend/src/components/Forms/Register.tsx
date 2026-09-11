1e649acdfd471037a38ed62e299ae61c880138cc
cammaj <cammaj@rpi.edu>
2026-09-11 19:22:20 -0400

    Update Register.tsx

--- frontend/src/components/Forms/Register.tsx
+++ frontend/src/components/Forms/Register.tsx
@@ -53,7 +53,7 @@ const formFields = [
     },
     {
         type: "checkbox",
-        label: "I understand that a $20 membership fee will be charged to my bursar account.",
+        label: "I understand a $20 membership fee applies if I submit a Machine Usage Form.",
         id: "bursar-acknowledgement",
         required: true,
     },
