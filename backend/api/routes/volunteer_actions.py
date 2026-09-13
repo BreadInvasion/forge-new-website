40cd655d6b3eac194997f2be516daa82c43ef13f
3791xk <xeniak3791@gmail.com>
2026-09-13 14:57:54 -0400

    Add failure email
    When a machine is failed it actually sends the machine usage failure email now

--- backend/api/routes/volunteer_actions.py
+++ backend/api/routes/volunteer_actions.py
@@ -1,18 +1,48 @@
 from datetime import datetime
 from typing import Annotated
+from azure.communication.email import EmailClient
 from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic import UUID4
 from sqlalchemy import select
 from sqlalchemy.orm import selectinload
 
+from core.config import settings
 from models.audit_log import AuditLog
 from models.machine import Machine
+from models.machine_usage import MachineUsage
 from models.user import User
 from schemas.enums import LogType, Permissions
 
 from ..deps import DBSession, PermittedUserChecker
 
 router = APIRouter()
+
+
+async def send_failure_email(email: str, machine_name: str):
+    client = EmailClient.from_connection_string(
+        settings.AZURE_COMMUNICATION_CONNECTION_STRING
+    )
+
+    message = {
+        "senderAddress": "DoNotReply@notifications.rpiforge.dev",
+        "recipients": {
+            "to": [{"address": email}]
+        },
+        "content": {
+            "subject": "Your Machine Usage Failed",
+            "plainText": (
+                f"Unfortunately, your machine usage on {machine_name} has failed. You may want to stop by the Forge and try again (A reprint is free)."
+            ),
+        },
+    }
+
+    poller = client.begin_send(message)
+    result = poller.result()
+    if result["status"] != "Succeeded" or result["error"] is not None:
+        raise HTTPException(
+            status_code=status.HTTP_502_BAD_GATEWAY,
+            detail="Failed to send machine usage failure email.",
+        )
 
 
 @router.post("/clear/{machine_id}")
@@ -69,7 +99,9 @@ async def fail_machine(
     machine = await session.scalar(
         select(Machine)
         .where(Machine.id == machine_id)
-        .options(selectinload(Machine.active_usage))
+        .options(
+            selectinload(Machine.active_usage).selectinload(MachineUsage.user)
+        )
     )
     if not machine:
         raise HTTPException(
@@ -96,3 +128,10 @@ async def fail_machine(
     machine.active_usage.failed_at = datetime.now()
     session.add(machine)
     await session.commit()
+
+    await send_failure_email(
+        f"{machine.active_usage.user.RCSID}@rpi.edu",
+        machine.name,
+    )
+
+    # TODO response
